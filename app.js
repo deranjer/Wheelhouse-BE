c56eb1a3fccbaf52997a11ed2f4d6a6c24e8a45d
Josh Gude <gude.joshua@gmail.com>
2019-05-14 13:32:27 -0400

    complete knex setup

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Knex = require('knex');
+const { Model } = require('objection');
 require('dotenv').config();
 
-const app = express();
+const knexfile = require('./knexfile');
 
+// Db Setup
+const env = process.env.NODE_ENV || 'development';
+const knex = Knex(knexfile[env]);
+Model.knex(knex);
+
+// App Setup
+const app = express();
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
--- knexfile.js
+++ knexfile.js
@@ -1,42 +1,11 @@
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+      database: process.env.devDatabase,
+      user: process.env.username,
+      password: process.env.password,
+      host: "127.0.0.1"
     }
   }
-
 };
