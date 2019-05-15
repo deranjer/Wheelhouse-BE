2250a7e893103de3eec5b9f2cfb09a3776b4d1ce
Josh Gude <gude.joshua@gmail.com>
2019-05-15 13:54:30 -0400

    Finished Competency objection model

--- models/Competency.js
+++ models/Competency.js
@@ -1,8 +1,38 @@
 const { Model } = require('objection');
+const User = require('objection');
 
 class Competency extends Model {
   static get tableName() {
     return 'competencies';
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['id', 'tag'],
+
+      properties: {
+        id: { type: 'integer' },
+        tag: { type: 'string', minLength: 1, maxLength: 40 },
+      },
+    };
+  }
+
+  static get relationMappings() {
+    return {
+      users: {
+        relation: Model.ManyToManyRelation,
+        modelClass: User,
+        join: {
+          from: 'competencies.id',
+          through: {
+            from: 'user_competencies.competency_id',
+            to: 'user_competencies.user_id',
+          },
+          to: 'users.id',
+        },
+      },
+    };
   }
 }
 
