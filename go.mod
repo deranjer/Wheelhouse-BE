5a9c115d7352f56447c1e9ab5a749683ebd047ac
deranjer <deranjer@gmail.com>
2019-07-01 22:23:55 -0400

    Updating testing files using fixtures for testing.

--- go.mod
+++ go.mod
@@ -5,10 +5,22 @@ module WheelhouseBE
 go 1.12
 
 require (
+	cloud.google.com/go v0.41.0 // indirect
 	github.com/alexedwards/scs/v2 v2.1.0
+	github.com/denisenkom/go-mssqldb v0.0.0-20190515213511-eb9f6a1743f3 // indirect
 	github.com/go-chi/chi v4.0.2+incompatible
 	github.com/go-chi/cors v1.0.0
 	github.com/go-chi/docgen v1.0.5
 	github.com/go-chi/render v1.0.1
+	github.com/lib/pq v1.1.1
+	github.com/mattn/go-oci8 v0.0.0-20190524004114-abcafa411cc8 // indirect
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+	golang.org/x/exp v0.0.0-20190627132806-fd42eb6b336f // indirect
+	golang.org/x/image v0.0.0-20190622003408-7e034cad6442 // indirect
+	golang.org/x/mobile v0.0.0-20190607214518-6fa95d984e88 // indirect
 	golang.org/x/net v0.0.0-20190628185345-da137c7871d7 // indirect
+	golang.org/x/sys v0.0.0-20190626221950-04f50cda93cb // indirect
+	golang.org/x/tools v0.0.0-20190701194522-38ae2c8f6412 // indirect
+	google.golang.org/genproto v0.0.0-20190701230453-710ae3a149df // indirect
+	gopkg.in/testfixtures.v2 v2.5.3
 )
--- main_test.go
+++ main_test.go
@@ -1,13 +1,57 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
+
+	_ "github.com/lib/pq"
+	"gopkg.in/testfixtures.v2"
 )
 
-func TestRoot(t *testing.T) {
-	req, err := http.NewRequest("GET", "/", nil)
+var (
+	db       *sql.DB
+	fixtures *testfixtures.Context
+)
+
+func DBSetup(m *testing.M) {
+	var err error
+	connStr := "user=postgres password=postgres port=5432 host=postgres dbname=wheelhouse-test "
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fixtures, err = testfixtures.NewFolder(db, &testfixtures.PostgreSQL{}, "testdata/fixtures")
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Exit(m.Run())
+}
+
+func PrepareTestDatabase() {
+	if err := fixtures.Load(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func TestDBConnection(t *testing.T) {
+	PrepareTestDatabase()
+	_, err := db.Query(`SELECT full_name FROM users WHERE id = 1`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func TestAPIGets(t *testing.T) {
+	APIBaseGet(t, "/", "Welcome to the root of WheelhouseBE") //Test Root
+}
+
+func APIBaseGet(t *testing.T, uri string, expected interface{}) {
+	req, err := http.NewRequest("GET", uri, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -26,7 +70,6 @@ func TestRoot(t *testing.T) {
 	}
 
 	// Check the response body is what we expect.
-	expected := "Welcome to the root of WheelhouseBE"
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body \n\tgot: %+v\n\twant: %+v",
 			rr.Body.String(), expected)
